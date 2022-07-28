e318029dba873bedb1d4306378acbc9d7cc5dd66
KelvinHalx <kelvinwainaina03@gmail.com>
2022-07-28 16:39:36 +0300

    App.js Product and Cart - Link Components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
-import Shop from './components/Shop'
+import Products from './components/Products';
+import Cart from './components/Cart';
+import { CartProvider, useCart } from "react-use-cart";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <Shop/>
-      </header>
+    <div className="container mt-2 mb-2">
+       <CartProvider>
+         <div className="row mt-2 mb-2">
+           <div className="col-md-8 mt-2 mb-2">
+              <Products/>
+           </div>
+           <div className="col-md-2 mt-2 mb-2">
+            <Cart/>
+           </div>
+
+         </div>
+      </CartProvider>
+
     </div>
   );
 }
 
 export default App;
+
+
