6e14e42ef83aa28a93c7bf5733ec126dbeb6792b
KelvinHalx <kelvinwainaina03@gmail.com>
2022-07-29 08:15:14 +0300

    Slider Implementation

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -10,6 +10,7 @@ function Cart() {
         items,
         updateItemQuantity,
         removeItem,
+        cartTotal
       } = useCart();
     
       if (isEmpty) return <p className="text-sm">Your cart is empty</p>;
@@ -23,16 +24,18 @@ function Cart() {
       <div className="row mt-4 mb-4">
 <div className="col-md-2"></div>
         <div className="col-md-8">
-    <h6 className="text-sm">Cart ({totalUniqueItems})</h6>
+    <h6 className="text-sm">Cart ({totalUniqueItems})
+    <span className="text-sm float-end">Cart total : ksh {cartTotal}</span></h6>
+
     
   <table class="table">
   <thead>
     <tr>
       <th scope="col"><p className="text-sm">Item</p></th>
       <th scope="col"><p className="text-sm">Quantity</p></th>
-      <th scope="col"><p className="text-sm">Add</p></th>
-      <th scope="col"><p className="text-sm">Remove</p></th>
-      <th scope="col"><p className="text-sm">Delete</p></th>
+      <th scope="col">Action</th>
+      <th scope="col"></th>
+      <th scope="col"></th>
     </tr>
   </thead>
   {items.map((item) => (
@@ -45,23 +48,22 @@ function Cart() {
         <p className="text-sm">{item.quantity}</p>
       </td>
       <td>
-      <button className="btn btn-outline-primary btn-sm"
+      <button className="btn btn-outline-primary btn-sm m-2"
             onClick={() => updateItemQuantity(item.id, item.quantity + 1)}
           >
             +
           </button>
-      </td>
-      <td>
-      <button className="btn btn-outline-danger btn-sm"
+
+      <button className="btn btn-outline-danger btn-sm m-2"
             onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
           >
             -
           </button>
-      </td>
-      <td>
-      <button className="btn btn-outline-danger btn-sm" onClick={() => removeItem(item.id)}>&times;</button>
+
+      <button className="btn btn-outline-danger btn-sm m-2" onClick={() => removeItem(item.id)}>&times;</button>
       </td>
     </tr>
+
   </tbody>
   ))}
 </table>
---
+++ src/components/Checkout.js
@@ -0,0 +1,17 @@
+iimport React from 'react'
+import { useCart } from 'react-use-cart';
+
+function Checkout() {
+        const {
+            isEmpty,
+            totalUniqueItems,
+            items,
+            updateItemQuantity,
+            removeItem,
+          } = useCart();
+  return (
+    <div>Checkout</div>
+  )
+}
+
+export default Checkout;
